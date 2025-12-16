c461a7e9b85edcf9b01ed64e5ea6cda407d93876
Kulsum <22156@ecam.be>
2025-12-16 01:01:14 +0100

    Création du CV : ajout des formations et projets

--- app/CV/page.tsx
+++ app/CV/page.tsx
@@ -4,22 +4,36 @@ export default function Cv() {
     return (
         <>
             <h1 className="text-center text-3xl my-8">Curriculum Vitae</h1>
+
+            <h2 className="text-2xl my-8">Formations</h2>
+            <CvEntry title="ECAM" date="2022-maintenant" description="Bachelier en Sciences de l’Ingénieur Industrielle - Secteur Génie Electrique">
+            </CvEntry>
+
+            <CvEntry title="Croix-Rouge de Belgique" date="2024-2029" description="BEPS - Brevet Européen Premiers Secours">
+            </CvEntry>
+
             <h2 className="text-2xl my-8">Expérience</h2>
-            <CvEntry title="Etudiante en informatique/électronique" employer="ECAM">
-                <ul>
-                    <li>Prend des cours</li>
-                </ul>
+            <CvEntry title="VFS Global" date="Mars 2022" description="Assistante & Relation client (stage)">
+                
             </CvEntry>
-            <CvEntry title="bbbb" employer="ECAM">
-                <ul>
-                    <li>Prend des cours</li>
-                </ul>
+
+            <h2 className="text-2xl my-8">Projets</h2>
+            <CvEntry title="Projet intégrateur" date="2022" description="Création d'une machine Low-Tech">
             </CvEntry>
-            <CvEntry title="hiri" employer="ECAM">
-                <ul>
-                    <li>Prend des cours</li>
-                </ul>
+
+            <CvEntry title="Modélisation mécanique - AutoCad" date="2022-2025" description="Conception de plans de pièces mécanique et lecture de plans">
             </CvEntry>
+
+            <CvEntry title="Développement de jeux en Python" date="2024-2025" description="Création d'une interface graphique avec Pygame et JSON (démineur) et conception d'un jeu en réseau TCP (quarto)">
+            </CvEntry>
+
+            <CvEntry title="PCB" date="2025-2026" description="Réalisation de cartes PCB avec Altium Designer">
+            </CvEntry>
+
+            <CvEntry title="Génie logiciel" date="2025-2026" description="Création d'une gestion de création de cassiers avec C#, en méthode Agile/Scrum et SQLite">
+            </CvEntry>
+            
+           
         </>
     )
 }
--- app/components/CvEntry.tsx
+++ app/components/CvEntry.tsx
@@ -1,21 +1,30 @@
 import { ReactNode } from "react";
 
 type CvEntry ={
-    title : string
-    employer : string
-    children : ReactNode
-}
+    title : string;
+    employer?: string;
+    children? : ReactNode;
+    date ?:string;
+    subtitle ?: string;
+    description: string;
+};
 
-export default function CvEntry(props : CvEntry) {
+export default function CvEntry({
+  title, employer, children, date, subtitle, description}: CvEntry){
   return (
     <div>
-        <div className="p-4 border border-fuchsia-200 rounded-xl shadow-sm mx-4 my-8">
+        <div className="p-4 border border-purple-300 rounded-2xl shadow-sm mx-4 my-8">
             <div className="flew items-center justify-between"></div>
-            <div>{props.title}</div>
-            <h3 className="text-xl font-bold text-amber-200">Etudiante</h3>
-            <h4 className="mb.2 italic text-amber-700">ECAM</h4>
+            <h3 className="text-xl font-bold text-purple-300">{title}</h3>
+
+            {date && (
+            <h4 className="mb.2 text-purple-900">{date}</h4>)}
+
+             {description && (
+            <p className="text-sm   text-purple-50">
+              {description}</p>)}
         </div>
-        (hey)
+        
     </div>
 
   )
