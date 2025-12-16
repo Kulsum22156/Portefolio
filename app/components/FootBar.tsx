a9dcdfc45c170ddda7d8962c90364af1f874ace4
Kulsum <22156@ecam.be>
2025-12-16 01:08:51 +0100

    Ajout d'un FootBar

---
+++ app/components/FootBar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Footbar() {
+  return (
+    <footer className="bg-black text-gray-400 py-6 mt-16">
+      <div className="max-w-5xl mx-auto px-6 flex justify-between">
+        <p>© 2025 Umme Kulsum</p>
+        <p>22156@ecam.be</p>
+        
+      </div>
+    </footer>
+  );
+}
