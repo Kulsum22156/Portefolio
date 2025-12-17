03ed54775d7fab7d3bba59fb55f484e56b137253
Kulsum <22156@ecam.be>
2025-12-18 00:30:49 +0100

    Création d'une table en SQLite

---
+++ app/db/database.ts
@@ -0,0 +1,12 @@
+import Database from "better-sqlite3";
+
+const db = new Database("blog.db");
+
+// Crée la table des blogs (une fois)
+db.prepare(`CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    date TEXT NOT NULL,
+    content TEXT NOT NULL)`).run();
+
+export default db;
