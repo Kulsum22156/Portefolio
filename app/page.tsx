e8f8e2c2e80910b47fb066e4d9b52230db27bf1c
Kulsum <22156@ecam.be>
2025-12-17 22:28:58 +0100

    Page d'accueil en responsible design

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main>
       <section className="min-h-[60vh] flex flex-col justify-center items-center text-center px-6">
-        <h1 className="text-4xl text-gray-300 font-bold">Umme Kulsum</h1>
+        <h1 className="text-4xl md:text-5xl text-gray-300 font-bold">Umme Kulsum</h1>
 
-        <p className="mt-4 text-lg text-gray-300">Étudiante en ingénierie industrielle – Secteur Génie électrique</p>
+        <p className="mt-4 text-lg md:text-2xl text-gray-300">Étudiante en ingénierie industrielle – Secteur Génie électrique</p>
 
-        <p className="mt-4 text-lg text-gray-300">En recherche de stage 🔍 </p>
+        <p className="mt-4 text-lg md:text-2xl text-gray-300">En recherche de stage 🔍 </p>
       </section>
     </main>
   );
