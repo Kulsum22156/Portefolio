6102c0881bb04ecb223d0f9f2df97931d8b302d5
Kulsum <22156@ecam.be>
2025-12-17 22:04:32 +0100

    Footbar en responsible design

--- app/components/FootBar.tsx
+++ app/components/FootBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footbar() {
   return (
     <footer className="bg-black text-gray-400 py-6 mt-16">
-      <div className="max-w-5xl mx-auto px-6 flex justify-between">
+      <div className="max-w-5xl mx-auto px-6 flex flex-col gap-2 text-center md:flex-row md:justify-between md:text-left">
         <p>© 2025 Umme Kulsum</p>
         <p>22156@ecam.be</p>
         
